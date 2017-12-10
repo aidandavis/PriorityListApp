34dfadfb4c6ef3b4fa53ebecd44421b50870b4f1
Aidan Davis <aidan.davis94@gmail.com>
2017-12-10 18:26:21 +0800

    bugs ironed, sorter working

--- app/src/main/java/com/aidandavisdev/aidandavis/prioritylist/PriorityListItemAdapter.kt
+++ app/src/main/java/com/aidandavisdev/aidandavis/prioritylist/PriorityListItemAdapter.kt
@@ -1,8 +1,8 @@
 package com.aidandavisdev.aidandavis.prioritylist
 
-import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 
@@ -12,28 +12,30 @@ import android.widget.TextView
 
 class PriorityListItemAdapter : RecyclerView.Adapter<PriorityListItemAdapter.ViewHolder>() {
 
-    class ViewHolder(view: CardView) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         internal var itemName: TextView = view.findViewById(R.id.item_name)
         internal var itemDescription: TextView = view.findViewById(R.id.item_description)
+        internal var itemScore: TextView = view.findViewById(R.id.item_score)
     }
 
     private var itemList: ArrayList<PrioritisedItem> = ArrayList()
 
     fun updateList(newList: List<PrioritisedItem>) {
         itemList.clear()
-        newList.sortedWith(compareBy(PrioritisedItem::getScore)).reversed() // sort according to priority score
-        itemList.addAll(newList)
+         // sort according to priority score
+        itemList.addAll(newList.sortedWith(compareBy(PrioritisedItem::getScore)).reversed())
         notifyDataSetChanged()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder? {
         var view = LayoutInflater.from(parent.context).inflate(R.layout.priority_item_card, parent, false)
-        return ViewHolder(view as CardView)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.itemName.text = itemList[position].name
         holder.itemDescription.text = itemList[position].description
+        holder.itemScore.text = "%.2f".format(itemList[position].getScore())
     }
 
     override fun getItemCount(): Int = itemList.size
