97472a63d218e20e106ef9ffe75e965ef1c6f484
Aidan Davis <aidan.davis94@gmail.com>
2017-12-16 13:13:08 +0800

    building new item activity

---
+++ app/src/main/java/com/aidandavisdev/aidandavis/prioritylist/NewItemActivity.kt
@@ -0,0 +1,18 @@
+package com.aidandavisdev.aidandavis.prioritylist
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by Aidan Davis on 16/12/2017.
+ */
+
+class NewItemActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_item)
+
+        
+    }
+}
