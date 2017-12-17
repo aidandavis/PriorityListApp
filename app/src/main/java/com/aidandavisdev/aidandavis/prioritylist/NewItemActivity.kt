88e775bdb55ff6b9913f7525bd6af5896d8298ef
Aidan Davis <aidan.davis94@gmail.com>
2017-12-17 14:06:09 +0800

    successful save to firestore

--- app/src/main/java/com/aidandavisdev/aidandavis/prioritylist/NewItemActivity.kt
+++ app/src/main/java/com/aidandavisdev/aidandavis/prioritylist/NewItemActivity.kt
@@ -6,7 +6,10 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_new_item.*
+import java.util.*
 
 /**
  * Created by Aidan Davis on 16/12/2017.
@@ -26,14 +29,41 @@ class NewItemActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_item)
+        val db = FirebaseFirestore.getInstance()
+        val uId = FirebaseAuth.getInstance().currentUser?.uid
 
-        item_create_button.setOnClickListener({ createItem() })
+        item_create_button.setOnClickListener({ createItem(db, uId) })
     }
 
-    private fun createItem() {
+    private fun createItem(db: FirebaseFirestore, uId: String?) {
         if (item_name.text.isEmpty()) {
             Toast.makeText(this, "Enter an item name", Toast.LENGTH_SHORT).show()
             return
+        }
+
+        if (uId != null) {
+            item_create_button.isEnabled = false
+            val item = HashMap<String, Any>()
+            item.put("name", item_name.text.toString())
+            item.put("description", item_description.text.toString())
+            item.put("importance", importance_seekbar.progress + 1)
+            item.put("urgency", urgency_seekbar.progress + 1)
+            item.put("effort", effort_seekbar.progress + 1)
+
+            db.collection("users")
+                    .document(uId)
+                    .collection("list1")
+                    .add(item)
+                    .addOnSuccessListener {
+                        Log.d(TAG, "Item added with ID: ${it.id}")
+                        finish()
+                    }
+                    .addOnFailureListener {
+                        Log.w(TAG, "Error adding item", it)
+                        Toast.makeText(this, "Failure adding item", Toast.LENGTH_SHORT).show()
+                    }
+
+            item_create_button.isEnabled = true
         }
 
         Log.i(TAG, "Name ${item_name.text}")
