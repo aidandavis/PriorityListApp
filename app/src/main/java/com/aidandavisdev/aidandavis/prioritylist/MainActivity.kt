686c66d527dcca5ac628bfde257ac8b0121f5344
Aidan Davis <aidan.davis94@gmail.com>
2018-01-14 18:24:13 +0800

    more ability to create new lists

--- app/src/main/java/com/aidandavisdev/aidandavis/prioritylist/MainActivity.kt
+++ app/src/main/java/com/aidandavisdev/aidandavis/prioritylist/MainActivity.kt
@@ -1,28 +1,34 @@
 package com.aidandavisdev.aidandavis.prioritylist
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.view.MenuItem
+import android.widget.EditText
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.dialog_new_list.*
 import java.util.*
 import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private val TAG = "MainActivity"
 
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mPriorityListAdapter: PriorityListItemAdapter
 
-    private val listSelected = "" // if empty list, then see all items ('master list')
+    private var listSelected = "" // if empty list, then see all items ('master list')
+    private val lists = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +43,8 @@ class MainActivity : AppCompatActivity() {
         val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
 
         main_list_view.setHasFixedSize(true)
         main_list_view.layoutManager = LinearLayoutManager(this)
@@ -60,18 +68,57 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         if (mAuth.currentUser != null) {
-            val itemList = ArrayList<PrioritisedItem>()
-            getItemsInList(mAuth.currentUser!!.uid, listSelected)
-                    .get()
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            task.result.mapTo(itemList) { mapToPrioritisedItem(it) }
-                            mPriorityListAdapter.updateList(itemList, listSelected)
-                        } else {
-                            Log.w(TAG, "Error getting items", task.exception)
-                        }
-                    }
+            updateItems()
+            updateLists()
         }
+    }
+
+    private fun updateLists() {
+        getUserDoc(mAuth.currentUser!!.uid)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        if (task.result.exists()) {
+                            val listMenu = nav_view.menu.addSubMenu("Lists")
+                            lists.clear()
+                            listMenu.clear()
+                            for (listName in task.result["lists"] as ArrayList<String>) {
+                                lists.add(listName)
+                                listMenu.add(listName)
+                            }
+                        }
+                    } else {
+                        Log.w(TAG, "Error getting lists", task.exception)
+                    }
+                }
+    }
+
+    private fun updateItems() {
+        val itemList = ArrayList<PrioritisedItem>()
+        getItemsInList(mAuth.currentUser!!.uid, listSelected)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        task.result.mapTo(itemList) { mapToPrioritisedItem(it) }
+                        mPriorityListAdapter.updateList(itemList, listSelected)
+                    } else {
+                        Log.w(TAG, "Error getting items", task.exception)
+                    }
+                }
+    }
+
+    private fun showTickedItems() {
+        val itemList = ArrayList<PrioritisedItem>()
+        getTickedItems(mAuth.currentUser!!.uid)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        task.result.mapTo(itemList) { mapToPrioritisedItem(it) }
+                        mPriorityListAdapter.updateList(itemList, listSelected)
+                    } else {
+                        Log.w(TAG, "Error getting items", task.exception)
+                    }
+                }
     }
 
     private fun mapToPrioritisedItem(result: DocumentSnapshot): PrioritisedItem {
@@ -86,6 +133,44 @@ class MainActivity : AppCompatActivity() {
                 result["ticked"] as Boolean)
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_list -> {
+                createAddListDialogue()
+            }
+            R.id.ticked_items -> {
+                showTickedItems()
+            }
+            R.id.master_list -> {
+                listSelected = ""
+                updateItems()
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    private fun createAddListDialogue() {
+        val builder = AlertDialog.Builder(this)
+        val dialogView = this.layoutInflater.inflate(R.layout.dialog_new_list, null)
+        val newNameEditText = dialogView.findViewById<EditText>(R.id.new_list_text)
+        builder.setView(dialogView)
+                .setPositiveButton("Create", { _, _ ->
+                    val enteredName = newNameEditText.text.toString()
+                    if (enteredName !== "") {
+                        lists.add(enteredName)
+                        getUserDoc(mAuth.currentUser!!.uid)
+                                .update("lists", lists)
+                        updateLists()
+                    }
+                })
+                .setNegativeButton("Cancel", { dialog, _ ->
+                    dialog.cancel()
+                })
+        builder.create().show()
+    }
+
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
@@ -96,10 +181,14 @@ class MainActivity : AppCompatActivity() {
 
 
     companion object {
-        fun getItemsCollection(uId: String): CollectionReference {
+        fun getUserDoc(uId: String): DocumentReference {
             return FirebaseFirestore.getInstance()
                     .collection("users")
                     .document(uId)
+        }
+
+        fun getItemsCollection(uId: String): CollectionReference {
+            return getUserDoc(uId)
                     .collection("items")
         }
 
@@ -110,9 +199,14 @@ class MainActivity : AppCompatActivity() {
 
         // empty list means get all lists
         fun getItemsInList(uId: String, list: String): Query {
-            return getItemsCollection(uId)
-                    .whereEqualTo("list", list)
-                    .whereEqualTo("ticked", false)
+            return if (list != "") {
+                getItemsCollection(uId)
+                        .whereEqualTo("list", list)
+                        .whereEqualTo("ticked", false)
+            } else {
+                return getItemsCollection(uId)
+                        .whereEqualTo("ticked", false)
+            }
         }
 
         fun getTickedItems(uId: String): Query {
